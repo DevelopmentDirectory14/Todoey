d26087362fa47405e609f1f6988fe80bf1238bd3
Aleksey Shapoval <alex@negativeimprint.com>
2019-10-14 17:30:17 -0400

    Added destructive full swipe functionality to category cells.

--- Todoey-2/Controllers/CategoryViewController.swift
+++ Todoey-2/Controllers/CategoryViewController.swift
@@ -134,7 +134,6 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
                     print("Error deleting category, \(error)")
                 }
                 
-                tableView.reloadData()
             }
             
         }
@@ -145,4 +144,10 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
         return [deleteAction]
     }
 
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
 }
