7fc0b062e7d16d59ca7ef0a44c242aac4b9b6647
Aleksey Shapoval <alex@negativeimprint.com>
2019-10-13 20:42:39 -0400

    Added ability to edit done status, editing entries in Realm and reloading tableView to see them.

--- Todoey-2/Data Model/Item.swift
+++ Todoey-2/Data Model/Item.swift
@@ -11,6 +11,6 @@ import RealmSwift
 
 class Item: Object {
     @objc dynamic var title : String = ""
-    @objc dynamic var done : Bool = true
+    @objc dynamic var done : Bool = false
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
